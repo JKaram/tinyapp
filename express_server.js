df4f781790e59928a1c15163d0d6d4d444f7d49b
Jamie <jamiekaram16@gmail.com>
2019-12-09 23:36:36 +0000

    Comments + nodemon

--- express_server.js
+++ express_server.js
@@ -1,5 +1,3 @@
-
-
 const express = require('express');
 const app = express();
 const PORT = 8080; 
@@ -15,45 +13,48 @@ const urlDatabase = {
   '7xc3lK' : 'http://tsn.ca'
 };
 
-
+// ----- Generate Random String ------ //
 function generateRandomString() {
   return Math.random().toString(36).slice(5)
  }
 
+// ----- Post a new URL page ------ //
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+// ----- Post new URL Redirect to check URL ------ //
 app.post("/urls", (req, res) => {
   const makeURL = generateRandomString()
-
   urlDatabase[makeURL] = req.body.longURL;
   console.log(urlDatabase);  // Log the POST request body to the console
   res.redirect(`/urls/${makeURL}`)
 });
 
-
+// ----- List of URLs ------ //
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+// ----- Check URL ------ //
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   console.log(req.params)
   res.render("urls_show", templateVars);
 });
 
+
+// ----- Redirect URL ------ //
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL]
+  if(longURL) {
+  console.log('log',longURL)
   res.redirect(longURL);
+  } 
 });
 
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`)
 });
-
-
-
-// console.log(generateRandomString())
