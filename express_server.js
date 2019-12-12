f9f9a34fe23b61d1f91d8989e4850ad13dcf8de1
Jamie <jamiekaram16@gmail.com>
2019-12-12 01:01:56 +0000

    Better comments and Linted

--- express_server.js
+++ express_server.js
@@ -2,7 +2,7 @@ const bcrypt = require('bcrypt');
 const express = require('express');
 const bodyParser = require("body-parser");
 const cookieParser = require('cookie-parser');
-const cookieSession = require('cookie-session')
+const cookieSession = require('cookie-session');
 
 const PORT = 8080;
 const app = express();
@@ -33,32 +33,31 @@ const users = {
   }
 };
 
-// Returns object of userid Urls
+//  ------ FUNCTION | RETURNS OBJECT OF USER ID'S URLS ------  //
 const urlsForUser = id => {
-  const urlKeys = Object.keys(urlDatabase)
+  const urlKeys = Object.keys(urlDatabase);
   let  usersUrlList = {};
   for (let i = 0; i < urlKeys.length; i++) {
     if (urlDatabase[urlKeys[i]].userID === id) {
-      usersUrlList[urlKeys[i]] = urlDatabase[urlKeys[i]]
+      usersUrlList[urlKeys[i]] = urlDatabase[urlKeys[i]];
     }
   }
   return usersUrlList;
-}
+};
 
-// Returns Users ID  of email that is entered
+//  ------ FUNCTION | RETURNS THE USER ID OF EMAIL PARAMETER ------  //
 const returnID = (object, email) => {
-  const keys = Object.keys(object)
+  const keys = Object.keys(object);
   for (let i = 0; i < keys.length; i++) {
     if (object[keys[i]].email === email) {
-      return keys[i]
+      return keys[i];
     }
   }
-
   return false;
 
 };
 
-// return true if email is already in use 
+//  ------ FUNCTION | RETURN TRUE IF EMAIL IS IN USE ------ //
 const checkEmailIsInUse = (object, email) => {
   const values = Object.values(object).filter(o => o.email === email);
 
@@ -68,15 +67,16 @@ const checkEmailIsInUse = (object, email) => {
 
   return false;
 };
-// ----- Generate Random String ------ //
-function generateRandomString() {
-  return Math.random().toString(36).slice(5)
-}
 
-// ----- new URL page ------ //
+// ----- FUNCTION | GENERATE UNIQUE ID ------ //
+const generateRandomString = () => {
+  return Math.random().toString(36).slice(5);
+};
+
+// ----- GET / MAKE NEW URL PAGE ------ //
 app.get("/urls/new", (req, res) => {
   if (!users[req.session.user_id]) {
-    res.redirect('/login')
+    res.redirect('/login');
   } else {
     let templateVars = { user: users[req.session.user_id].email };
     res.render("urls_new", templateVars);
@@ -84,42 +84,37 @@ app.get("/urls/new", (req, res) => {
 
 });
 
-// ----- Post new URL Redirect to check URL ------ //
+// ----- POST / MAKE NEW SHORT URL && REDIRECT TO SHOW & EDIT SHORT URL PAGE  ------ //
 app.post("/urls", (req, res) => {
-  const makeURL = generateRandomString()
+  const makeURL = generateRandomString();
   urlDatabase[makeURL] = { longURL: req.body.longURL, userID: req.session.user_id };
-
-  console.log('CREATED New URL', urlDatabase);  // Log the POST request body to the console
-  res.redirect(`/urls/${makeURL}`)
+  res.redirect(`/urls/${makeURL}`);
 });
 
-// ----- List of URLs Page ------ //
+// ----- GET / URLS PAGE ------ //
 app.get("/urls", (req, res) => {
   if (req.session.user_id) {
-  let templateVars = { urls: urlsForUser(req.session.user_id) , user: users[req.session.user_id].email };
-  console.log('Main Page')
-  res.render("urls_index", templateVars);
+    let templateVars = { urls: urlsForUser(req.session.user_id) , user: users[req.session.user_id].email };
+    res.render("urls_index", templateVars);
   } else {
     res.status(404).send(`NICE TRY HACKER`);
   }
 });
 
 
-// ----- Register Page ------ //
+// ----- GET / REGISTER PAGE ------ //
 app.get("/register", (req, res) => {
   let templateVars = { user: users[req.session.user_id] };
   res.render("urls_register", templateVars);
 });
 
-// ----- Register A new account ------ //
+// ----- POST / REGISTER A NEW ACCOUNT ------ //
 app.post("/register", (req, res) => {
-  console.log('REGISTER', req.body.email, req.body.password);
-  const idNum = generateRandomString()
+  const idNum = generateRandomString();
 
   if (!checkEmailIsInUse(users, req.body.email) && req.body.password) {
     users[idNum] = { id: idNum, email: req.body.email, password: bcrypt.hashSync(req.body.password, 10) };
     req.session.user_id =  idNum;
-    console.log('users[idNum]', idNum)
     res.redirect('/urls');
   } else {
     res.status(400).send(`Either your email is in use. Or the password feild is blank`);
@@ -127,15 +122,14 @@ app.post("/register", (req, res) => {
 
 });
 
-// ----- Login Page ------ //
+// ----- GET / LOGIN PAGE ------ //
 app.get("/login", (req, res) => {
   let templateVars = { user: users[req.session.user_id] };
   res.render("urls_login", templateVars);
 });
 
-// ----- Login to account ------ //
+// ----- POST / LOGIN  ------ //
 app.post("/login", (req, res) => {
-  console.log('LOGIN', req.body.email, req.body.password);
   if (req.body.email === '' || req.body.password === '') {
     res.status(401).send(`One of these feilds are blank`);
   }
@@ -152,34 +146,28 @@ app.post("/login", (req, res) => {
 });
 
 
-
-// ----- Show URL ------ //
+// ----- GET / SHOW && EDIT SHORT URL PAGE ------ //
 app.get("/urls/:shortURL", (req, res) => {
-  console.log('SHOW URL', req.params.shortURL)
   if (users[req.session.user_id]) {
     let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL].longURL , user: users[req.session.user_id].email };
-    console.log('TEMPLATE VARS------>', templateVars)
     res.render("urls_show", templateVars);
   } else {
     res.redirect('/login');
   }
-  
 });
 
 
-// ----- LOGOUT ------ //
+// ----- POST / LOGOUT ------ //
 app.post("/logout", (req, res) => {
   req.session = null;
-  res.redirect('/login')
+  res.redirect('/login');
 });
 
 
 // ----- Redirect URL ------ //
 app.get("/u/:shortURL", (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL].longURL
-  console.log(longURL)
+  const longURL = urlDatabase[req.params.shortURL].longURL;
   if (longURL) {
-    console.log('REDIRECT')
     res.redirect(longURL);
   }
 });
@@ -187,8 +175,8 @@ app.get("/u/:shortURL", (req, res) => {
 // ----- Delete URL ------ //
 app.post("/urls/:shortURL/delete", (req, res) => {
   if (req.session.user_id) {
-  delete urlDatabase[req.params.shortURL];
-  res.redirect(`/urls`)
+    delete urlDatabase[req.params.shortURL];
+    res.redirect(`/urls`);
   } else {
     res.status(404).send(`NICE TRY HACKER`);
   }
@@ -197,15 +185,13 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 
 // ----- EDIT URL ------ //
 app.post("/urls/:shortURL", (req, res) => {
-  console.log('EDIT', req.params.shortURL, req.body.editURL);
   urlDatabase[req.params.shortURL] = { longURL: req.body.editURL, userID: req.session.user_id };
-  console.log(urlDatabase)
-  res.redirect(`/urls`)
+  res.redirect(`/urls`);
 });
 
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`)
+  console.log(`Example app listening on port ${PORT}!`);
 });
 
 
